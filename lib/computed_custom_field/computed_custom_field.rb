b025407101c40380bf4394c8cd5853d75137dbb0
bilel-kedidi <bilel.kedidi@gmail.com>
2020-05-04 08:31:27 +0100

    add order line

--- lib/computed_custom_field/computed_custom_field.rb
+++ lib/computed_custom_field/computed_custom_field.rb
@@ -4,6 +4,20 @@ module ComputedCustomField
       Enumeration, Group, Issue, Project,
       TimeEntry, User, Version
     ]
+    models << Expense if defined?(Expense)
+    models << Invoice if defined?(Invoice)
+    models << InvoicePayment if defined?(InvoicePayment)
+    models << InvoiceLine if defined?(InvoiceLine)
+    models << Contact if defined?(Contact)
+    models << Deal if defined?(Deal)
+    models << ProductLine if defined?(ProductLine)
+    models << Note if defined?(Note)
+    models << Account if defined?(Account)
+    models << Person if defined?(Person)
+    models << WkAttendance if defined?(WkAttendance)
+    models << Wktime if defined?(Wktime)
+
+
     models.each do |model|
       if model.included_modules
               .exclude?(ComputedCustomField::ModelPatch)
